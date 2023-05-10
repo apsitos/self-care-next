f568975a052fa27458f0d85156788ff232ef1dcd
Annastasia Psitos <annastasia.psitos@gmail.com>
2023-05-10 15:47:23 -0600

    set radio functionality

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
+'use client';
+
+import { useState } from 'react';
+
 import Image from 'next/image';
 
 import meditate from '../images/meditate.svg';
 
 export default function Home() {
+  const [quoteType, setQuoteType] = useState('');
+
+  const handleRadio = (e) => {
+    setQuoteType(e.target.id);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24 text-white">
       <h1 className="text-center text-3xl font-bold">✨ Self Care Center ✨</h1>
@@ -11,25 +21,39 @@ export default function Home() {
       <div className="bg-white py-4 px-5 w-1/2 rounded-lg flex flex-col">
         <div className="flex justify-around">
           <div>
-            <input type="radio" id="affirmation" className="mr-2.5" />
+            <input
+              type="radio"
+              id="affirmation"
+              name="quote-type"
+              value="affirmation"
+              className="mr-2.5"
+              onClick={handleRadio}
+            />
             <label htmlFor="affirmation" className="text-black italic">
               affirmation
             </label>
           </div>
           <div>
-            <input type="radio" id="mantra" className="mr-2.5" />
+            <input
+              type="radio"
+              id="mantra"
+              name="quote-type"
+              value="mantra"
+              className="mr-2.5"
+              onClick={handleRadio}
+            />
             <label htmlFor="mantra" className="text-black italic">
               mantra
             </label>
           </div>
         </div>
         <div className="flex justify-center">
-          <button className="bg-dark-blue rounded-md px-5 mt-3 w-48">
+          <button className="bg-dark-blue rounded-md px-5 mt-4 w-48">
             Receive Message
           </button>
         </div>
       </div>
-      <div className="bg-white rounded-lg mt-32 py-12 w-5/6 flex justify-center">
+      <div className="bg-white rounded-lg mt-32 py-12 w-2/3 flex justify-center">
         <Image
           src={meditate}
           alt="clip art meditating person"
