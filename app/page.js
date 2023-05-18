10d6d3dd8851624655ae04a741c105c8c1b04a32
Annastasia Psitos <annastasia.psitos@gmail.com>
2023-05-18 15:10:05 -0600

    add sayings and display

--- app/page.js
+++ app/page.js
@@ -2,25 +2,34 @@
 
 import { useState } from 'react';
 
+import { affirmations } from '@/data/affirmations';
+import { mantras } from '@/data/mantras';
+
 import QuoteDisplay from './QuoteDisplay';
 import RadioInput from './RadioInput';
 
 export default function Home() {
   const [quoteType, setQuoteType] = useState('');
+  const [saying, setSaying] = useState(null);
 
   const handleRadio = (e) => {
     setQuoteType(e.target.id);
   };
 
-  const getMessage = () => {};
+  const getMessage = () => {
+    const selection = Math.floor(Math.random() * (15 - 1) + 1);
+    return setSaying(
+      quoteType === 'affirmation' ? affirmations[selection] : mantras[selection]
+    );
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 text-white">
-      <h1 className="text-center text-3xl font-bold">✨ Self Care Center ✨</h1>
+    <main className='flex min-h-screen flex-col items-center p-24 text-white'>
+      <h1 className='text-center text-3xl font-bold'>✨ Self Care Center ✨</h1>
 
-      <h2 className="font-bold text-xl mt-20 mb-5">Which type of message?</h2>
+      <h2 className='font-bold text-xl mt-20 mb-5'>Which type of message?</h2>
       <RadioInput handleRadio={handleRadio} getMessage={getMessage} />
-      <QuoteDisplay />
+      <QuoteDisplay saying={saying} />
     </main>
   );
 }
